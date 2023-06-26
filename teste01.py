bb4a7aaf50c7d4d23ee9403ce76b40e3ea4b5825
Yuri <yuriribeirodeoliveira@gmail.com>
2023-06-26 17:24:32 -0300

    Iniciando exercicios do curso em video de Python

---
+++ teste01.py
@@ -0,0 +1,21 @@
+# -*- coding: cp1252 -*-
+
+nome = input('Qual é o seu nome?\n')
+print("===  DESAFIO 01  ===")
+print("Boas vindas " + nome)
+print("\n")
+
+dia = input('Qual dia você nasceu?\n')
+mes = input('Em que mês?\n')
+ano = input('E em qual ano?\n')
+print("===  DESAFIO 02  ===")
+print("Data nascimento: " + dia + "/" + mes + "/" + ano)
+print("\n")
+
+x = input("Insira um numero: ")
+y = input("Insira outro número: ")
+print("O resultado é: ", int(x) + int(y))
+print("\n")
+
+
+
